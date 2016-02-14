5fab61a275a5149f82aa8fd853e21f81eb407b8c
shelleyyap <shelley_yap@hotmail.com>
2016-02-14 21:36:03 +0800

    Tested isTxtFile

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -1,4 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TextBuddyTest {
-
+	@Test
+	public void TextBuddyTesting(){
+		String[] fileNama = new String[1];
+		fileNama[0] = "file.txt";
+		assertTrue(TextBuddy.isTxtFile(fileNama));
+	}
 }
